358d75ae48d06109da602428a669157203cb3962
deric <github@mail.xingjian.me>
2017-04-07 11:57:57 +0800

    minor

--- src/components/ArticleLink/index.js
+++ src/components/ArticleLink/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import TimeAgo from 'react-timeago';
 
-const ArticleLink = ({ id, headline, category, owner, create_date, onClick }) => {
+const ArticleLink = ({ headline, category, pub_date, onClick }) => {
   return (
     <li className="list-group-item">
+      <span className="home-category">{category}</span>
+      {' '}
       <span className="home-headline" ><a href="#" onClick={onClick}>{headline}</a></span>
       {' '}
-      <span className="home-category">{category}</span>
+      <TimeAgo className="home-pubdate" date={pub_date} />
     </li>
   );
 };
